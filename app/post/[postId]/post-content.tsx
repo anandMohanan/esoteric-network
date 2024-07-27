ce13ef8d656ae51f1ae1aa8b62a2801c69dbf3b2
anandMohanan <ananthkvmohanan@gmail.com>
2024-07-27 11:08:39 +0530

    Added a back button

--- app/post/[postId]/post-content.tsx
+++ app/post/[postId]/post-content.tsx
@@ -1,22 +1,34 @@
 "use client"
 
-import { Section } from "@/components/dividers";
+import { Container, Section } from "@/components/dividers";
 import { MinimalTiptapEditor } from "@/components/editor";
+import { Button } from "@/components/ui/button";
 import { primaryfont } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
+import { ChevronLeftIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 
 export const PostContent = ({ content }: { content: string }) => {
+    const router = useRouter()
     return (
-        <Section className="md:w-[800px] w-full flex items-center mx-auto border-none">
-            <MinimalTiptapEditor
-                onValueChange={() => { }}
-                outputValue="json"
-                contentClass={cn("mx-auto border-none ", primaryfont.className)}
-                disabled={true}
-                initialContent={content}
-            />
+        <Section>
+            <Container>
+                <Button onClick={() => router.back()} variant={"outline"}>
+
+                    <ChevronLeftIcon className="h-5 w-5" />
+                </Button>
+            </Container>
+            <Container className="md:w-[800px] w-full flex items-center mx-auto border-none">
+                <MinimalTiptapEditor
+                    onValueChange={() => { }}
+                    outputValue="json"
+                    contentClass={cn("mx-auto border-none ", primaryfont.className)}
+                    disabled={true}
+                    initialContent={content}
+                />
+            </Container>
         </Section>
     )
 }
